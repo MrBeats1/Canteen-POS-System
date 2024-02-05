61036d776ed939a73c529096101a0c95f9754a21
Nathan <nathananjelo.b@gmail.com>
2024-02-05 18:04:58 +0800

    c

--- index/script.js
+++
@@ -1,6 +0,0 @@
-function toggleDropdown() {
-    var dropdownContent = document.getElementById("myDropdown");
-    dropdownContent.style.display === "none"
-      ? (dropdownContent.style.display = "block")
-      : (dropdownContent.style.display = "none");
-  }
